d1739aaf1772a5bf4d9702c33c91b1c13b3532b7
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-03-29 07:06:59 +0545

    Add: Settingup the Frontend

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <main
+      className='h-screen w-full bg-gray-950 border-2 border-red-700 flex gap-4 p-4'>
+        <aside
+          className='h-screen w-1/4 bg-amber-50 rounded-lg' 
+          >
+
+        </aside>
+
+        <section
+          className='h-screen w-3/4 bg-neutral-800 rounded-lg'
+        >
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
